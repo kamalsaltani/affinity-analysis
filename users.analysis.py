cffde337bd2a9b8c489a1f0762b7ca8770be88a8
Ryan Poplin <ryan.poplin@turner.com>
2016-05-20 02:55:22 -0400

    working condition with keen.io

--- users.analysis.py
+++ users.analysis.py
@@ -3,17 +3,23 @@
 from keen.client import KeenClient
 
 # set keen.io keys
+def get_keen_client():
+	return KeenClient(
+		project_id = "",
+		write_key = "",
+		read_key = ""
+	)
 
 import numpy as np
 
 dataset_filename = "users.dataset.txt"
 X = np.loadtxt(dataset_filename) # TODO: look into making this JSON instead of text...
 
-# ex: article that has a video, a like button, a share button, 
-# dataset: 5 items == liked, shared, scrolledToBottom, videoFinished, adClicked 
+# ex: article that has a video, a like button, a share button,
+# dataset: 5 items == liked, shared, scrolledToBottom, videoFinished, adClicked
 
 # support: support is the number of times a rule occurs in a dataset. (count the total of occurences)
-# confidence: how accurate they are. (how valid is the rule we're trying to enforce) 
+# confidence: how accurate they are. (how valid is the rule we're trying to enforce)
 
 # if a user likes an article, they also share that article
 
@@ -26,7 +32,7 @@ for sample in X:
 	if sample[1] == 1: # person shared an article
 		num_article_shares += 1
 
-print(X[:5]) # quantity of users and their data
+# print(X[:100]) # quantity of users and their data
 
 # we'd like to be able to get our users who like an article to share that article
 print("{0} people liked an article".format(num_article_likes)) # data condition match
@@ -41,7 +47,7 @@ invalid_rules = defaultdict(int)
 num_occurences = defaultdict(int)
 
 for sample in X:
-	# premise in range(4) is referring to out of 5 pieces of data per user, 
+	# premise in range(4) is referring to out of 5 pieces of data per user,
 	# we want to see if it happened: 1 or not: 0
 	for premise in range(4): # config proper range for data
 		if sample[premise] == 0: continue
@@ -66,13 +72,13 @@ def print_rule(premise, conclusion, support, confidence, features):
 	print(" - Support: {0}".format(support[(premise, conclusion)]))
 	print(" - Confidence: {0:.3f}".format(confidence[(premise, conclusion)]))
 	# send data off to keen.io
-	# keen.add.event("affinity_analysis", {
-	# 	"num_article_likes": num_article_likes
-	# 	"num_article_shares": num_article_shares 
-	# 	"analysis_rule": "If a person likes an article, they will also share that article"
-	# 	"user_support": support[(premise, conclusion)]
-	# 	"rule_confidence": format(confidence[(premise, conclusion)], ".3f")
-	# })
+	client = get_keen_client()
+	client.add_event("affinity_analysis", {
+		"likes": num_article_likes,
+		"shares": num_article_shares,
+		"support": support[(premise, conclusion)],
+		"confidence": format(confidence[(premise, conclusion)], ".3f")
+	})
 
 premise = 0 # if a person liked an article,
 conclusion = 1 # that person will also share that article.
