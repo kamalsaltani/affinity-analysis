17096b53cf78561ce3377a20709d06075eb5c66e
Byrdann Fox <byrdannfox@gmail.com>
2016-05-22 22:03:45 -0400

    prereq math stuff...

---
+++ math/start.math.py
@@ -0,0 +1,33 @@
+# how to add and subtract in Python
+print(1 + 2)
+print(1 + 3.5)
+print(-1 + 2.5)
+print(100 - 45)
+print(-1.1 + 5)
+
+# how to multiply in Python
+print(3 * 2)
+print(3.5 * 1.5)
+
+# how to divide in Python
+print(3 / 2)
+print(4 / 2)
+
+# floor division in Python
+print(3 // 2)
+print(-3 // 2) # very cool!
+
+# modulo in Python
+print(9 % 2)
+
+# exponentials in Python
+print(2 ** 2)
+print(2 ** 10)
+print(2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2)
+# or you could loop above which is what (**) operator is doing under the covers
+print(1 ** 10)
+
+# square root
+print(8 ** (1/2))
+# cube root
+print(8 ** (1/3))
